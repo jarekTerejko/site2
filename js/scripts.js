2eabd36a8d761d655cce9ed107e938bbd0987f18
Jarosław Terejko <j.terejko@gmail.com>
2017-10-13 18:44:50 +0200

    added new features

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
-var slideIndex = 0;
-showSlides();
+$(document).ready(function ($) {
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
+    $('.my-slider').unslider({
+        autoplay: true,
+        infinite: true,
+        arrows: false
+    });
+
+    $('a[href^="#"]').on('click', function (event) {
+
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1500);
+        }
+    });
+
+
+});
